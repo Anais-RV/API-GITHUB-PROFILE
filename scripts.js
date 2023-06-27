d95c2cd7afa28ba1c510664867c7310e585bc3f1
Anais-RV <anaisrv@gmail.com>
2023-06-27 21:44:22 +0200

    updated user card, not displayed before searching an user

--- scripts.js
+++ scripts.js
@@ -10,20 +10,31 @@ const followers = document.getElementById("followers");
 const following = document.getElementById("following");
 const repos = document.getElementById("repos");
 
-
-
 const getUser = async (username) => {
   try {
+    if (!username) {
+      main.style.display = "none";
+      return;
+    }
+
     const response = await axios.get(API_URL + username);
     const usuario = response.data;
 
-    avatar.src = `${usuario.avatar_url || "NO PIC"}`;
+    avatar.src = `${usuario.avatar_url}`;
     usernameElement.innerHTML = usuario.login;
-    bio.innerHTML = `Bio: ${usuario.bio || "No vio available"}`;
+    bio.innerHTML = `Bio: ${usuario.bio || "No bio available"}`;
     followers.innerHTML = `Followers: ${usuario.followers}`;
     following.innerHTML = `Following: ${usuario.following}`;
     repos.innerHTML = `Repos: ${usuario.public_repos}`;
 
+    const userLink = document.createElement("a");
+    userLink.href = usuario.html_url;
+    userLink.target = "_blank";
+    userLink.innerHTML = "GitHub Profile";
+
+    main.appendChild(userLink);
+
+    main.style.display = "block";
   } catch (error) {
     console.log(error);
   }
@@ -35,3 +46,7 @@ form.addEventListener("submit", (event) => {
   const username = searchBox.value;
   getUser(username);
 });
+
+main.style.display = "none";
+
+
