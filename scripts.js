e015ceb057a66a69ed23faa9c3b18dbbcf9a5364
Anais-RV <anaisrv@gmail.com>
2023-06-28 10:38:25 +0200

    refactoring DOM

--- scripts.js
+++ scripts.js
@@ -2,13 +2,20 @@ const API_URL = "https://api.github.com/users/";
 const main = document.getElementById("main");
 const searchBox = document.getElementById("search");
 
-const avatar = document.getElementById("avatar");
-const usernameElement = document.getElementById("username");
-const bio = document.getElementById("bio");
+// Función para crear los elementos dinamicamente
 
-const followers = document.getElementById("followers");
-const following = document.getElementById("following");
-const repos = document.getElementById("repos");
+const createUserElement = (tagName, id, className = "") => {
+  const element = document.createElement(tagName);
+  if (id) {
+    element.id = id;
+  }
+  if (className) {
+    element.className = className;
+  }
+  return element;
+};
+
+// API, conecto
 
 const getUser = async (username) => {
   try {
@@ -20,6 +27,22 @@ const getUser = async (username) => {
     const response = await axios.get(API_URL + username);
     const usuario = response.data;
 
+    main.innerHTML = ""; // Limpiar contenido previo
+
+    // Creo los elementos para la card
+
+    const card = createUserElement("div", "", "card");
+    const avatar = createUserElement("img", "avatar", "avatar");
+    const userInfo = createUserElement("div", "", "user-info");
+    const usernameElement = createUserElement("h2", "username");
+    const bio = createUserElement("p", "bio");
+    const followers = createUserElement("p", "followers");
+    const following = createUserElement("p", "following");
+    const repos = createUserElement("p", "repos");
+    const userLink = createUserElement("a", "", "user-link");
+
+    // Asigno contenido
+
     avatar.src = `${usuario.avatar_url}`;
     usernameElement.innerHTML = usuario.login;
     bio.innerHTML = `Bio: ${usuario.bio || "No bio available"}`;
@@ -27,18 +50,34 @@ const getUser = async (username) => {
     following.innerHTML = `Following: ${usuario.following}`;
     repos.innerHTML = `Repos: ${usuario.public_repos}`;
 
-    const userLink = document.createElement("a");
     userLink.href = usuario.html_url;
     userLink.target = "_blank";
     userLink.innerHTML = "GitHub Profile";
 
-    main.appendChild(userLink);
+    // Inserto
+
+    userInfo.appendChild(usernameElement);
+    userInfo.appendChild(bio);
+    userInfo.appendChild(followers);
+    userInfo.appendChild(following);
+    userInfo.appendChild(repos);
+    userInfo.appendChild(userLink);
+    
+
+    card.appendChild(avatar);
+    card.appendChild(userInfo);
+
+    // Agrego tarjeta del usuario
+
+    main.appendChild(card);
 
     main.style.display = "block";
   } catch (error) {
     console.log(error);
   }
 };
+
+// Buscador de usuario
 
 const form = document.getElementById("form");
 form.addEventListener("submit", (event) => {
@@ -47,6 +86,10 @@ form.addEventListener("submit", (event) => {
   getUser(username);
 });
 
+// Ocultar la card hasta que tenga búsqueda
+
 main.style.display = "none";
+
+
 
 
